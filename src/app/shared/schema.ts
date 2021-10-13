e4e89f6c9379638a47b946f36eabf28047e06734
SpringIntoJava <springintojava@gmail.com>
2021-10-13 11:29:36 +0300

    delete unnecessary schema.ts file

--- src/app/shared/schema.ts
+++
@@ -1,8 +0,0 @@
-export class Schema{
-private baseUrl = "http://localhost:8080/dlist/";
-
-
-    public getSubmitterDavenfors(email:string){
-        return (this.baseUrl + 'getmynames/'+email);
-    }
-}
