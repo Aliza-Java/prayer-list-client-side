aa97a58489689e77f02b0f07756322b3f497a448
SpringIntoJava <springintojava@gmail.com>
2021-10-13 13:17:21 +0300

    initial structure for settings component

--- src/app/admin/admin-settings/admin-settings.component.ts
+++ src/app/admin/admin-settings/admin-settings.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Admin } from 'src/app/shared/models/admin.model';
+import { AdminService } from '../admin.service';
 
 @Component({
-  selector: 'app-admin-settings',
-  templateUrl: './admin-settings.component.html',
-  styleUrls: ['./admin-settings.component.css']
+    selector: 'app-admin-settings',
+    templateUrl: './admin-settings.component.html',
+    styleUrls: ['./admin-settings.component.css']
 })
 export class AdminSettingsComponent implements OnInit {
+    settingsForm: FormGroup;
+    admin: Admin = null;
+    constructor(public adminService: AdminService) { }
 
-  constructor() { }
+    ngOnInit() {
+        this.admin = this.adminService.adminLogin;
+        this.settingsForm = new FormGroup({
+            'email': new FormControl(this.admin.email, [Validators.required, Validators.email]),
+            'prompt': new FormControl(this.admin.newNamePrompt),
+            'wait': new FormControl(this.admin.waitBeforeDeletion)
+        });
+    }
 
-  ngOnInit() {
-  }
-
+    onSubmit(){
+        alert('submitted!  Now make the changes on the DB!');
+    }
 }
