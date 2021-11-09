78bcf3201b120bc4c064756ec70737ee6905de90
SpringIntoJava <springintojava@gmail.com>
2021-11-09 12:55:35 +0200

    design page-not-found

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-not-found',
-  templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.css']
+    selector: 'app-not-found',
+    templateUrl: './not-found.component.html',
+    styleUrls: ['./not-found.component.css']
 })
 export class NotFoundComponent implements OnInit {
 
-  constructor() { }
+    constructor(public router: Router) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+    homePage() {
+        this.router.navigate(['/']);
+    }
 
 }
