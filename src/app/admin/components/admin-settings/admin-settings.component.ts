6de8a0fac9acfdd6e3503d32f2bebdcd481afb9e
SpringIntoJava <springintojava@gmail.com>
2025-03-30 13:07:10 +0300

    small validation adjustments to admin-settings component

--- src/app/admin/components/admin-settings/admin-settings.component.ts
+++ src/app/admin/components/admin-settings/admin-settings.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { UntypedFormControl, UntypedFormGroup} from '@angular/forms';
+import { UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { AdminService } from '../../admin.service';
@@ -14,34 +14,45 @@ import { DaveningService } from 'src/app/shared/services/davening.service';
 export class AdminSettingsComponent implements OnInit, OnDestroy {
     settingsForm: UntypedFormGroup = new UntypedFormGroup({});
     prompt: boolean = false;
-    wait: number = 7;
+    wait: number = 0;
     settingsUpdatedSub: Subscription = new Subscription;
-    
-    constructor(public daveningService:DaveningService, public adminService: AdminService, public router: Router, public authService: AuthService) { }
+    disableSubmit: boolean = false;
+
+    constructor(public daveningService: DaveningService, public adminService: AdminService, public router: Router, public authService: AuthService) { }
 
     async ngOnInit() {
         this.adminService.populateAdminSettings();
         this.populateSettingsForm();
 
         this.settingsUpdatedSub = this.adminService.settingsUpdated.subscribe(adminSettings => {
-            this.prompt = adminSettings.newNamePrompt ?? false;
-            this.wait = adminSettings.waitBeforeDeletion ?? 7;
+            this.prompt = adminSettings.newNamePrompt ?? this.prompt;
+            this.wait = adminSettings.waitBeforeDeletion ?? this.wait;
         });
     }
 
     populateSettingsForm() {
         this.settingsForm = new UntypedFormGroup({
             'prompt': new UntypedFormControl(this.adminService.adminSettings.newNamePrompt),
-            'wait': new UntypedFormControl(this.adminService.adminSettings.waitBeforeDeletion)
+            'wait': new UntypedFormControl(this.adminService.adminSettings.waitBeforeDeletion, [Validators.required, Validators.pattern(/^\d+$/)]) //prevent negative or decimals. also null value (empty) is still considered a value (valid for 'required')
         });
     }
 
     onSubmit() {
-        this.adminService.editSettings(this.adminService.adminSettings.email ?? '', this.prompt, this.wait);
+        if (this.wait < 0 || this.wait == null) {
+            this.disableSubmit = true;
+            return;
+        }
+        else {
+            this.adminService.editSettings(this.adminService.adminSettings.email ?? '', this.prompt, this.wait);
+        }
     }
 
     onCancel() {
         this.router.navigate(['admin/names']);
+    }
+
+    checkWaitInput() {
+        this.disableSubmit = (this.wait < 0 || this.wait == null);
     }
 
     ngOnDestroy() {
